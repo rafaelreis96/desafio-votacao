2f18313b560b9f3a7a3be86586bf09387b29cf4d
Rafael Reis <rafaelreisdeoliveira12@gmail.com>
2024-05-16 19:21:12 -0400

    add config versoes api doc

--- src/main/java/dev/rafaelreis/desafiovotacao/config/OpenAPIConfig.java
+++ src/main/java/dev/rafaelreis/desafiovotacao/config/OpenAPIConfig.java
@@ -3,6 +3,7 @@ package dev.rafaelreis.desafiovotacao.config;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,9 +18,30 @@ public class OpenAPIConfig {
                     .title("VOTACAO REST API")
                     .description("API para votação de pautas em assembleias de associados")
                     .version("1.0")
+                    .version("2.0")
                     .license(new License()
                         .name("Apache License Version 2.0")
                         .url("https://www.apache.org/licenses/LICENSE-2.0")));
+    }
+
+    @Bean
+    public GroupedOpenApi apiV1() {
+        String[] paths = {"/api/v1/**"};
+        return GroupedOpenApi.builder()
+                .group("v1")
+                .pathsToMatch(paths)
+                .pathsToExclude("/api/v2/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi apiV2() {
+        String[] paths = {"/api/v2/**"};
+        return GroupedOpenApi.builder()
+                .group("v2")
+                .pathsToMatch(paths)
+                .pathsToExclude("/api/v1/**")
+                .build();
     }
 
 }
